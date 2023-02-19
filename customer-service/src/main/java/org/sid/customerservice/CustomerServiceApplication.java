d361ee730bca27f081dceb171b696a90c3a9181f
Ouma1ma <72311680+Ouma1ma@users.noreply.github.com>
2023-02-19 19:53:21 +0100

    add customer-service

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,28 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            RepositoryRestConfiguration restConfiguration) {
+        restConfiguration.exposeIdsFor(Customer.class);
+        return args -> {
+            customerRepository.save(new Customer(null,"oumaima","oumaima@gmail.com"));
+            customerRepository.save(new Customer(null,"hasnae","hasnae@gmail.com"));
+            customerRepository.save(new Customer(null,"mohamed","mohamed@gmail.com"));
+        };
+    }
+}
---
+++ customer-service/src/main/java/org/sid/customerservice/entities/Customer.java
@@ -0,0 +1,40 @@
+package org.sid.customerservice.entities;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
